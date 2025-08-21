7e50b8775deaad1f1ee6e2530a7e67e44dc9db37
Gerardo Ramos <gerx_37@hotmail.com>
2025-08-21 11:31:00 -0600

    Added coloring to the grid on hover

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ function generateSquares() {
     for (let index = 0; index < 256; index++) {
         const square = document.createElement("div");
         square.className = "square";
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = getRandomColor();
+        });
         grid.appendChild(square);
     }
+}
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 generateSquares();
