d1b8e8972796c8f06a2597c22f5d2f8b2c526bfc
Gerardo Ramos <gerx_37@hotmail.com>
2025-08-21 11:50:31 -0600

    Added button to create new grid

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-function generateSquares() {
+function generateSquares(squaresPerSide) {
     const grid = document.querySelector("#grid");
+    const totalSquares = squaresPerSide * squaresPerSide;
+    const squareSide = 100 / squaresPerSide;
 
-    for (let index = 0; index < 256; index++) {
+    for (let index = 0; index < totalSquares; index++) {
         const square = document.createElement("div");
         square.className = "square";
+
+        square.style.width = `${squareSide}%`;
+        square.style.height = `${squareSide}%`;
         square.addEventListener("mouseover", () => {
             square.style.backgroundColor = getRandomColor();
         });
@@ -20,4 +25,28 @@ function getRandomColor() {
     return color;
 }
 
-generateSquares();
+function cleanGrid() {
+    const grid = document.querySelector("#grid");
+    grid.innerHTML = "";
+}
+
+function createGrid() {
+    let squaresPerSide = 0;
+
+    while (squaresPerSide > 100 || squaresPerSide < 1) {
+        squaresPerSide = parseInt(
+            prompt(
+                "Enter the number of squares per side for the new grid (Max. 100)",
+                "16"
+            )
+        );
+    }
+
+    cleanGrid();
+    generateSquares(squaresPerSide);
+}
+
+const newGridBtn = document.querySelector("button");
+newGridBtn.addEventListener("click", createGrid);
+
+generateSquares(16);
